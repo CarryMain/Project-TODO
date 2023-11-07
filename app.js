ef7a46c6e2c628292c3b54041a3ab349ae291ec1
CarryMain <mr.turanov98@gmail.com>
2023-11-07 18:47:02 +0300

    add logic

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-const onUserInput = document.querySelector('.inputTask'),
-      onBtnTask = document.querySelector('.btnTask'),
-      container = document.querySelector('.container');
+const   onUserInput = document.querySelector('.inputTask'),
+        onBtnTask = document.querySelector('.btnTask'),
+        container = document.querySelector('.container');
+
+let tasks = [];
 
 onBtnTask.addEventListener('click', () => {
-const inputText = onUserInput.value.trim();
+let inputText = onUserInput.value.trim();
 
-if (!inputText) {
-    alert('You need to write something!');
-    return;
-}
+    if (!inputText) {
+        alert('You need to write something!');
+        return;
+    }
 
-const div = document.createElement('div');
+    if(inputText.length > 50) {
+        inputText = inputText.slice(0, 50) + '...'
+    }
 
-const checkbox = document.createElement('input');
-checkbox.type = 'checkbox';  
-checkbox.className = 'todoTaskButton';
+    const div = document.createElement('div');
 
-div.classList.add('todoTask');
-div.append(checkbox);
-div.append(inputText);
-div.style.display = 'block';
-container.append(div);
-onUserInput.value = '';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';  
+    checkbox.className = 'todoTaskButton';
+
+    const taskText = document.createElement('span');
+    taskText.textContent = inputText;
+
+    div.classList.add('todoTask');
+    div.append(checkbox);
+    div.append(taskText);
+    div.style.display = 'block';
+    container.append(div);
+    onUserInput.value = '';
+    tasks.push(div);
+
+    const footerTask = document.querySelector('.footerTask');
+
+    footerTask.innerHTML = ` 
+        <div id="oneHover" class="itemsLeft">${tasks.length} item(s) left</div>
+        <div class="moreBtn">
+        <div id="oneHover" class="activeA">All</div>
+        <div id="oneHover" class="activeB">Active</div>
+        <div id="oneHover" class="activeC">Completed</div>
+        </div>
+        <div id="oneHover" class="clearCompleted">Clear Completed</div>
+    `;
 });
 
-
-
-
+container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('todoTaskButton')) {
+        const currentCheckbox = event.target;
+        const parentDiv = currentCheckbox.parentElement;
+        const taskText = parentDiv.querySelector('span');
+        taskText.style.textDecoration = currentCheckbox.checked ? 'line-through' : 'none';
+    }
+});
